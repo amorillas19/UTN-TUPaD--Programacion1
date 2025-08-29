69b6e5b71030d107fc4e60169d782b85029fcb46
Andres Morillas <60206580+amorillas19@users.noreply.github.com>
2025-08-28 21:28:59 -0300

    actualizacion tp4

--- Unidad 04 Estructuras Repetitivas/TP4.py
+++ Unidad 04 Estructuras Repetitivas/TP4.py
@@ -1,1 +1,33 @@
-print("hola mundo")
+import random
+
+numeroElegido=int(random.randint(0,101))
+print(numeroElegido)
+
+flag : bool = bool(True)
+flagInterna : bool = bool (True)
+contadorIntentos : int=int(1)
+
+
+
+
+while(flag==True):
+    
+    print("Adivine el numero:")
+
+    while(flagInterna==True):
+        numeroUsuario=(input())
+        if(isinstance(numeroUsuario, int)):
+            flagInterna==False
+            print("paso por aca")
+
+    if (numeroUsuario==numeroElegido):
+        print("correcto")
+        print("Acertaste en ", contadorIntentos, " intentos")
+        flag=False
+    elif(numeroUsuario>numeroElegido):
+        print("El numero debe ser mas chico")
+        contadorIntentos+=1
+    elif(numeroUsuario<numeroElegido):
+        print("El numero debe ser mas grande")
+        contadorIntentos+=1
+
