aeaf744f9c88208d2ee66925e3511b312871ae32
Andres Morillas <60206580+amorillas19@users.noreply.github.com>
2025-09-16 17:42:21 -0300

    finalizacion TP5

--- Unidad 04 Estructuras Repetitivas/test.py
+++ Unidad 04 Estructuras Repetitivas/test.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-numero_usuario=int(input("Por favor ingrese un numero entero para comenzar la suma. Ingrese 0 para finalizar: "))
-suma_total=numero_usuario
-
-while (numero_usuario != 0):
-    numero_usuario=int(input("Por favor ingrese un numero entero para agregar a la suma. Ingrese 0 para finalizar: "))
-    suma_total=suma_total+numero_usuario
-
-
-print("La suma total de los enteros ingresados es: " , suma_total)
-=======
 #Ejercicio 10
 
 numero_usuario=(int(input("Ingrese el numero, para invertir el orden de los digitos: ")))
@@ -21,4 +10,3 @@ while(numero_usuario>0):
     numero_usuario=numero_usuario//10
 
 print(numero_invertido)
->>>>>>> 493d8fdfb73a30f42980f723091a28fc020fd1fc
--- Unidad 05 Listas/Testeo Sorting.py
+++
@@ -1,6 +0,0 @@
-lista : list=[2,3,4,10,20,66,17,12,23,45,345,1]
-
-lista.sort()
-
-for i in lista:
-    print (i)
