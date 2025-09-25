89ca50b73958c486e354be83d481cae161316b17
Andres Morillas <60206580+amorillas19@users.noreply.github.com>
2025-09-25 13:19:33 -0300

    Parcial 24/09

--- Simulacion Parcial 01/parcial1.py
+++ Simulacion Parcial 01/parcial1.py
@@ -1,128 +1,113 @@
-lista_libros=[]
-lista_ejemplares=[]
+titulos=[]
+ejemplares=[]
 
-opcion_usuario=0
+menu_mostrar=True
 
-while(opcion_usuario!=10):
-    print("---------------")
-    print("MENU:")
-    print("1- Ingresar nombre del titulo")
-    print("2- Ingresar cantidad de ejemplares del titulo")
-    print("3- Mostrar catalogo de libros")
-    print("4- Ingrese el libro a buscar")
-    print("5- Listar libros con copias disponibles")
-    print("6- Agregar titulo y sus copias")
-    print("7- Ver titulos agotados")
-    print("8- Actualizar ejemplares")
-    print("9- Salir")
-    opcion_usuario=input("Ingrese la opcion que desea utilizar: ")
+while (menu_mostrar):
+    print("MENU")
+    print('''
+          1- Ingresar titulos
+          2- Ingresar ejemplares
+          3- Mostrar catalogo
+          4- Consultar disponibilidad
+          5- Listar agotados
+          6- Agregar titulo y ejemplares
+          7- Actualizar ejemplares(prestamo/devolucion)
+          8- Salir del menu''')
     
-    while(not(opcion_usuario.isdigit())):
-          print("La opcion ingresada no es un numero")
-          opcion_usuario=input("Ingrese la opcion que desea utilizar: ")
-
-    opcion_usuario=int(opcion_usuario)
+    opcion_usuario=int(input("Ingrese que opcion desea utilizar: "))
 
     
     if(opcion_usuario==1):
-        aux_titulo=input("Por favor ingrese el nombre de la obra: ")
-        aux_titulo=aux_titulo.lower()
+        menu_agregar=True
 
-        check_validacion=True
+        while(menu_agregar):
+            aux_name=(input("Ingrese el nombre de la obra: "))
+            if(not(aux_name=="")):
+                titulos.append(aux_name)
+                decision_usuario=(input("Desea agregar otro libro? S/N"))
+                decision_usuario=decision_usuario.upper()
+                if(decision_usuario=="N"):
+                    menu_agregar=False
+                    print("Gracias!")
+            else:
+                print("El titulo no es valido, o esta vacio")
 
-        for i in range(0, len(lista_libros)):
-            if(aux_titulo==lista_libros[i]):
-                check_validacion=False
-            elif(aux_titulo==""):
-                check_validacion=False
-
-        if(check_validacion):
-            print("El libro ha sido agregado a la lista")
-            lista_libros.append(aux_titulo)
-        else:
-            print("El libro NO ha sido agregado a la lista")
-
+    
     elif(opcion_usuario==2):
-        aux_cantidad=int(input("Ingrese la cantidad de ejemplares: "))
-        lista_ejemplares.append(aux_cantidad)
-
-    elif(opcion_usuario==3):
-        if(len(lista_ejemplares)==len(lista_libros)):
-            for i in range(0, len(lista_libros)):
-                print (lista_libros[i], ": ", lista_ejemplares[i], " copias")
-            else:
-                print("Las listas no son iguales")
-        
-
-    elif(opcion_usuario==4):
-        aux_busqueda=(input("Ingrese el libro a buscar:"))
-        aux_busqueda=aux_busqueda.lower()
-        for i in range(0, len(lista_libros)):
-            if(aux_busqueda==lista_libros[i]):
-                print(lista_ejemplares[i], " copias disponibles")
-            else:
-                print("No esta en la lista")
-
-    elif(opcion_usuario==5):
-        for i in range(0, len(lista_ejemplares)):
-                if(lista_ejemplares[i]>0):
-                    print(lista_libros[i], "tiene stock")
+        for i in range(0, len(titulos)):
+            check_cantidad=True
+            while(check_cantidad):
+                print ("El libro", titulos[i], "cuantos ejemplares tiene: ")
+                aux_cantidad=int(input())
+                if(aux_cantidad>=0):
+                    check_cantidad=False
+                    ejemplares.append(aux_cantidad)
+                    print("Cantidad de ejemplares agregada!")
                 else:
-                     print("NO tiene stock")
-
-    elif(opcion_usuario==6):
-        aux_nombre=input("Ingrese nombre de la obra: ")
-        aux_ejemplares=int(input("Ingrese cantidad de ejemplares: "))
-        lista_libros.append(aux_nombre)
-        lista_ejemplares.append(aux_ejemplares)
-
-    elif(opcion_usuario==7):
-        for i in range(0, len(lista_ejemplares)):
-            if(lista_ejemplares[i]==0):
-                print(lista_libros[i], "NO tiene stock")
-
-    elif(opcion_usuario==8):
-        print("Que opcion desea utilizar?")
-        print("A - Prestamo")
-        print("B - Devolucion")
-        opcion_prestamos=input("Que opcion desea utilizar?")
-        opcion_prestamos=opcion_prestamos.lower()
-
-        if(opcion_prestamos=="a"):
-            aux_reemplazo=input("Que libro desea llevar en prestamo? ")
-            aux_reemplazo=aux_reemplazo.lower()
-            for i in range(0, len(lista_libros)):
-                if(aux_reemplazo==lista_libros[i]):
-                    if(lista_ejemplares[i]>0):
-                        print("Se saca el libro")
-                        print("-----------------")
-                        lista_ejemplares[i]=lista_ejemplares[i]-1
-                        print("Ahora quedan ", lista_ejemplares[i], " copias")
-                    elif(lista_ejemplares[i]==0):
-                        print("No quedan suficientes libros para prestar")              
-        elif(opcion_prestamos=="b"):
-            aux_reemplazo=input("Que libro desea devolver? ")
-            aux_reemplazo=aux_reemplazo.lower()
-
-            for i in range(0, len(lista_libros)):
-
-                print("pasa por el bucle")
-                
-                if(aux_reemplazo==lista_libros[i]):
-                    print("Se devuelve el libro")
-                    print("-----------------")
-                    lista_ejemplares[i]=lista_ejemplares[i]+1
-                    print("Ahora quedan ", lista_ejemplares[i], " copias")
+                    print("No ha ingresado un numero de ejemplares valido")
+    
+    elif(opcion_usuario==3):
+        for i in range (0, len(titulos)):
+            print (f"Libro: {titulos[i]}. {ejemplares[i]} ejemplares")
+    
+    elif(opcion_usuario==4):
+        aux_busqueda=(input("Ingrese el titulo de la obra: "))
+        if aux_busqueda in titulos:
+            aux_indice=titulos.index(aux_busqueda)
+            print(f"La obra {titulos[aux_indice]} posee {ejemplares[aux_indice]} ejemplares")
         else:
-            print("Opcion incorrecta")
-            break
+            print("La obra no existe en la biblioteca")
+    
+    elif(opcion_usuario==5):
+        for i in range(0, len(titulos)):
+            if(ejemplares[i]==0):
+                print(f"El titulo {titulos[i]} no posee ejemplares")
+    
+    elif(opcion_usuario==6):
+        aux_doble_name=(input("Ingrese el nombre de la obra: "))
+        
+        if aux_doble_name in titulos:
+            print("El titulo ingresado ya existe en la biblioteca.")
+        else:
+            print("Ingrese la cantidad de ejemplares: ")
+            aux_doble_cantidad=(input("Ingrese la cantidad de ejemplares: "))
+            titulos.append(aux_doble_name)
+            ejemplares.append(aux_doble_cantidad)
+    
+    elif(opcion_usuario==7):
+        opcion_movimiento=(input("Desea solicitar un prestamo, o hacer una devolucion"))
 
-
-    elif(opcion_usuario==9):
-        print("Gracias")
+        if(opcion_movimiento=="prestamo"):
+            aux_prestamo_libro=(input("Que titulo desea solicitar en prestamo: "))
+            for i in range(0, len(titulos)):
+                if(aux_prestamo_libro==titulos[i]):
+                    print("El libro existe")
+                    if(ejemplares[i]>=1):
+                        ejemplares[i]-=1
+                        print("El libro ha sido prestado con exito")
+                        print("Cantidad de ejemplares restantes: ", ejemplares[i])
+                    else:
+                        print("No hay copias para llevar en prestamo")
+        
+        elif(opcion_movimiento=="devolucion"):
+            aux_devolucion_libro=(input("Que titulo desea devolver: "))
+            aux_devolucion_libro=aux_devolucion_libro.lower()
+            if aux_devolucion_libro in titulos:
+                print("El libro existe en la biblioteca")
+                indice_libro=titulos.index(aux_devolucion_libro)
+                ejemplares[indice_libro]+=1
+                print("El libro ha sido devuelto con exito")
+                print("Cantidad de ejemplares restantes: ", ejemplares[indice_libro])
+            else:
+                print("El titulo no se encuentra en la biblioteca")
+        
+        else:
+            print("No ha ingresado una opcion valida")
+    
+    elif(opcion_usuario==8):
         break
-
-    else:
-        print("No es una opcion valida")
     
 
+
+print("Gracias")
