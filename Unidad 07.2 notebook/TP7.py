41e2faa13f6138f43cb1d338c67badf8d9ccfc26
Andres Morillas <60206580+amorillas19@users.noreply.github.com>
2025-10-15 11:25:44 -0300

    Subir el ejercicio 4 del tp7

--- Unidad 07.2 notebook/TP7.py
+++ Unidad 07.2 notebook/TP7.py
@@ -22,8 +22,28 @@ print(precios_frutas)
 print("-------------------")
 
 #Ejercicio 3
-lista_random=precios_frutas.keys()
+lista_llaves=precios_frutas.keys()
 
 print("Ejercicio 3 Lista llaves")
-print(lista_random)
+print(lista_llaves)
 print("-------------------")
+
+#Ejercicio 4
+
+diccionario_telefonos={}
+
+while True:
+    nombre=input("Ingrese el nombre: ")
+    telefono=input("Ingrese el telefono: ")
+    diccionario_telefonos[nombre]=telefono
+    if (len(diccionario_telefonos)>4):
+        break
+
+nombre_busqueda=input("Ingrese el nombre de la persona que desea ubicar?")
+if(diccionario_telefonos.get(nombre_busqueda)):
+    print(f"El nombre {nombre_busqueda} tiene el telefono: {diccionario_telefonos[nombre_busqueda]}")
+else:
+    print(f"El nombre {nombre_busqueda} NO tiene telefono")
+
+#Ejercicio 5
+
