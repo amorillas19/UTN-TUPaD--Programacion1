00ff362f7f06f15355d013508608a7084a4a7bd0
Andres Morillas <patomorillas@gmail.com>
2025-10-16 19:14:38 -0300

    Remover la branch de la notebook

--- Unidad 06 Funciones/test.py
+++ Unidad 06 Funciones/test.py
@@ -1,29 +1,5 @@
-#Ejercicio 10
+def prueba (a=10):
+    print (a+1)
 
-def calcular_promedio(a,b,c):
 
-    promedio=(a+b+c)/3
-    return promedio
-
-numero_1=input("Ingrese el numero 1: ")
-if(not(numero_1.isdigit)):
-    while(not(numero_1.isdigit)):
-        numero_1=input("Lo ingresado no es valido. Ingrese el numero 1: ")
-else:
-    numero_1=float(numero_1)
-
-numero_2=input("Ingrese el numero 2: ")
-if(not(numero_2.isdigit)):
-    while(not(numero_2.isdigit)):
-        numero_2=input("Lo ingresado no es valido. Ingrese el numero 2: ")
-else:
-    numero_2=float(numero_2)
-
-numero_3=input("Ingrese el numero 3: ")
-if(not(numero_3.isdigit)):
-    while(not(numero_3.isdigit)):
-        numero_3=input("Lo ingresado no es valido. Ingrese el numero 3: ")
-else:
-    numero_3=float(numero_3)
-
-print(f"El promedio es de {calcular_promedio(numero_1, numero_2, numero_3)}")
+prueba(20)   
