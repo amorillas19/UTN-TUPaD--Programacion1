9b1c10dabd47f32ddb1a25a88e347e46b789830c
Andres Morillas <patomorillas@gmail.com>
2025-10-17 11:42:17 -0300

    Finalizacion tp7

--- Unidad 07 Datos Complejos/TP7.py
+++ Unidad 07 Datos Complejos/TP7.py
@@ -83,3 +83,109 @@ for i in diccionario_alumnos:
         promedio+=diccionario_alumnos[i][j]
     print(f"La nota promedio de {i} es de: {promedio/3}") 
 
+#Ejercicio 7
+
+parcial1 = {1, 2, 3, 4, 5}
+parcial2 = {4, 5, 6, 7, 8}
+
+ambos = parcial1 & parcial2
+solo_uno = parcial1 ^ parcial2
+al_menos_uno = parcial1 | parcial2
+
+print("Aprobaron ambos:", ambos)
+print("Aprobaron solo uno:", solo_uno)
+print("Aprobaron al menos uno:", al_menos_uno)
+
+#Ejercicio 8
+
+supermercado={"manzana": 700, "banana":500, "huevo":120, "fideos":1000}
+
+while True:
+    print('''Que desea hacer?
+          1-Consultar stock de un producto
+          2- Agregar cantidad de stock a un producto
+          3- Agregar producto al supermercado.
+          4- Salir:''')
+    opcion=input()
+
+    if(opcion=="1"):  
+        stock_consulta=input("De que producto deseas consultar stock? ")
+        while(not(stock_consulta.isalpha())):
+            stock_consulta=input("Ingreso invalido. De que producto deseas consultar stock? ")
+        stock_consulta.lower()
+        if stock_consulta in supermercado:
+            print(supermercado[stock_consulta])
+        else:
+            print("El producto no existe")
+
+    if(opcion=="2"): 
+        stock_agregar=input("De que producto deseas agregar stock? ")
+        while(not(stock_agregar.isalpha())):
+                stock_agregar=input("Ingreso invalido. De que producto deseas agregar stock? ")
+        stock_agregar.lower()
+        if stock_agregar in supermercado:
+            cantidad_agregar=input("Cuantas unidades desea agregar al stock? ")
+            if(not(cantidad_agregar.isdigit())):
+                while(not(cantidad_agregar.isdigit())):
+                    cantidad_agregar=input("Debe ingresar un numero valido por favor: ")
+            else:
+                cantidad_agregar=int(cantidad_agregar)
+                supermercado[stock_agregar]+=cantidad_agregar
+                print(f"La cantidad de stock ha sido actualizada. Ahora el producto {stock_agregar} tiene {supermercado[stock_agregar]} unidades")
+        else:
+                print("El producto no existe")
+
+    if(opcion == "3"): 
+        producto_agregar = input("Que producto desea agregar al supermercado? ")
+        while (not (producto_agregar.isalnum())):
+            print ("Nombre invalido, por favor ingreselo correctamente: ")
+        if producto_agregar in supermercado:
+            print ("Producto ya existente")
+        else:
+            print("El producto no está en el supermercado.")
+            producto_cantidad=input("Que cantidad tiene el producto a agregar? ")
+            while(not(producto_cantidad.isdigit())):
+                producto_cantidad=input("Cantidad no valida. Por favor ingrese la cantidad del producto a agregar: ")
+            producto_cantidad=int(producto_cantidad)
+            supermercado[producto_agregar]=producto_cantidad
+            print("Producto agregado exitosamente")
+            print(supermercado)
+    
+    if(opcion=="4"):
+        print("Gracias!")
+        break
+
+#Ejercicio 9
+
+agenda = {("lunes","13"):"Reunion", 
+          ("martes","15"):"Asado", 
+          ("jueves","20"):"Viaje", 
+          ("viernes","10"):"Despedida"}
+
+dia = input("En que dia desea consultar la actividad? ").lower()
+hora = input("En que hora se realizará la actividad? ")
+
+tupla_busqueda = (dia, hora)
+
+if tupla_busqueda in agenda:
+    print (agenda[tupla_busqueda])
+else:
+    print ("No hay actividades para ese dia y horario")
+
+#Ejercicio 10
+#Ejercicio 10
+
+original = {"Argentina": "Buenos aires", 
+            "Brasil": "Brasilia", 
+            "Chile":"Santiago", 
+            "Colombia": "Bogota"}
+
+invertido = {}
+
+for i in original:
+    key=original[i]
+    value=i
+    tupla=(key, value)
+    invertido.update({tupla})
+
+print(invertido)
--- Unidad 07 Datos Complejos/test.py
+++ Unidad 07 Datos Complejos/test.py
@@ -1,2 +1,16 @@
-#Ejercicio 7
+#Ejercicio 10
 
+original = {"Argentina": "Buenos aires", 
+            "Brasil": "Brasilia", 
+            "Chile":"Santiago", 
+            "Colombia": "Bogota"}
+
+invertido = {}
+
+for i in original:
+    key=original[i]
+    value=i
+    tupla=(key, value)
+    invertido.update({tupla})
+
+print(invertido)
