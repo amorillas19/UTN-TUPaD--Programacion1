d7edf7f154da06e535806f174c4062465d6524e0
Andres Morillas <patomorillas@gmail.com>
2025-10-21 12:11:31 -0300

    TP 8 finalizado

--- Unidad 08 Manejo de archivos/test.py
+++ Unidad 08 Manejo de archivos/test.py
@@ -16,3 +16,39 @@ with open ("ejer1-productos.txt", "a") as archivo:
     precio :str = input("Ingrese el precio")
     cantidad :str = input("Ingrese la cantidad")
     archivo.write(f"{producto},{precio},{cantidad}\n")
+
+#Ejercicio 4
+
+lista_diccionario = []
+
+for item in lista_productos:
+    item=item.strip().split(",")
+    producto_d={}
+    producto_d["nombre"]=item[0]
+    producto_d["precio"]=item[1]
+    producto_d["cantidad"]=item[2]
+    lista_diccionario.append(producto_d)
+
+print(lista_diccionario)
+
+#Ejercicio 5
+
+nombre_buscar :str = input("Ingrese el nombre del producto a buscar: ")
+flag :bool = False
+ 
+for item in lista_diccionario:
+    
+    if  nombre_buscar == item["nombre"]:
+        print (item)
+        flag = True
+
+if flag == False:
+        print ("El item no está en la lista")   
+     
+
+#Ejercicio 6
+
+with open ("ejer1-productos.txt", "w") as archivo:
+     
+    for item in lista_diccionario:
+        archivo.write(f"{item["nombre"]},{item["precio"]},{item["cantidad"]}\n")
